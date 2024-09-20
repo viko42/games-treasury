e60817e00bf436b59b28128d8fbeaf36991926fe
viko42 <vlancien.paris@gmail.com>
2024-09-20 16:05:59 +0100

    chore: typo

--- app/runiverse/charts/commission-chart.tsx
+++ app/runiverse/charts/commission-chart.tsx
@@ -56,7 +56,7 @@ export default function Component(props: { stats: StatsType }) {
       <CardHeader className="flex items-center gap-2 space-y-0 border-b border-gray-700 py-5 sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
           <CardDescription className="text-gray-300">
-            Showing total commissions every day for the last 1 month
+            Showing total commissions every day
           </CardDescription>
         </div>
         <Select value={timeRange} onValueChange={setTimeRange}>
